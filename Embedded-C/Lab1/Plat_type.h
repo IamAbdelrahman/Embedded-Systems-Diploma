edb4060b95205efdcec0106a1180d4da21336ece
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2024-03-26 16:51:43 +0200

    Lab 1 Embedded C

--- Embedded-C/Lab1/Plat_type.h
+++ Embedded-C/Lab1/Plat_type.h
@@ -2,7 +2,7 @@
  * Platforms_types.h
  *
  *  Created on: Dec 12, 2022
- *      Author: Abdelrhaman Kamal
+ *  Author: Abdelrhaman Kamal
  */
 
 #ifndef PLATFORMS_TYPES_H_
@@ -16,35 +16,35 @@
 #endif
 
 #ifndef FALSE
-#define FALSE	(boolean)false
+#define FALSE			(boolean)false
 #endif
 
 #ifndef TRUE
-#define TRUE	(boolean)true
+#define TRUE			(boolean)true
 #endif
 
-#define CPU_TYPE	CPU_TYPE_64
-#define CPU_BIT_ORDER	MSB_FIRST
-#define CPU_BYTE_ORDER	HIGH_BYTE_FIRST
+#define CPU_TYPE		CPU_TYPE_64
+#define CPU_BIT_ORDER		MSB_FIRST
+#define CPU_BYTE_ORDER		HIGH_BYTE_FIRST
 
-typedef _Bool	boolean;
-typedef unsigned char uint8_t;
-typedef signed char	  int8_t;
+typedef _Bool			boolean;
+typedef unsigned char 		uint8_t;
+typedef signed char	  	int8_t;
 typedef short	int16_t;
-typedef unsigned short	uint16_t;
-typedef int	int32_t;
-typedef unsigned int	uint32_t;
-typedef long long	int64_t;
+typedef unsigned short		uint16_t;
+typedef int			int32_t;
+typedef unsigned int		uint32_t;
+typedef long long		int64_t;
 typedef unsigned long long	uint64_t;
 
-typedef int8_t	sint8_t;
-typedef uint8_t	uint8_t;
-typedef char	char_t;
-typedef int16_t	sint16_t;
-typedef int32_t	sint32_t;
-typedef uint32_t	uint32_t;
-typedef int64_t	sint64_t;
-typedef uint64_t	uint64_t;
+typedef int8_t			sint8_t;
+typedef uint8_t		uint8_t;
+typedef char			char_t;
+typedef int16_t		sint16_t;
+typedef int32_t		sint32_t;
+typedef uint32_t		uint32_t;
+typedef int64_t		sint64_t;
+typedef uint64_t		uint64_t;
 
 typedef volatile int8_t	vint8_t;
 typedef volatile uint8_t	vuint8_t;
