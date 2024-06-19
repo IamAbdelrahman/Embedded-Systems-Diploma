c10fbece4c8033c6b063af310e22824ea1fbc87d
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2024-06-20 00:26:21 +0300

    KEYPAD Driver

---
+++ Essential-Peripherals/Lab6/HAL/KEYPAD/KEYPAD.c
@@ -0,0 +1,123 @@
+/******************************************************************************
+ * Copyright (C) 2024 by Abdelrahman Kamal - Learn-In-Depth Diploma
+ *****************************************************************************/
+/**
+ * @file KEYPAD.c
+ * @brief The implementation for the KEYPAD driver
+ *
+ * @author Abdelrahman Kamal
+ *
+ */
+
+/**********************************************************************
+ * Includes
+ **********************************************************************/
+#include "KEYPAD.h"
+
+/**********************************************************************
+ * Preprocessor Macros
+ **********************************************************************/
+
+/**********************************************************************
+ * Typedefs
+ **********************************************************************/
+
+/**********************************************************************
+ * Module Variables
+ **********************************************************************/
+const vuint8_t Keypad_Rows[] = {ROW0, ROW1, ROW2, ROW3};
+const vuint8_t Keypad_Cols[] = {COL0, COL1, COL2, COL3};
+
+/**********************************************************************
+ * Function Definitions
+ **********************************************************************/
+
+/**********************************************************************
+ * Function : KEYPAD_Init()
+ *
+ *  Description:
+ *  This function is used to initialize the KEYPAD device
+ *  through setting the row pins as output and columns pins as input.
+ *  PRE-CONDITION: Configuration table needs to populated (sizeof > 0)
+
+ *  @return void
+ **********************************************************************/
+void KEYPAD_Init() {
+  DIO_Init(KEYPAD_ConfigGet());
+  DIO_PinWrite(KEYPAD_PORT, ROW0, HIGH);
+  DIO_PinWrite(KEYPAD_PORT, ROW1, HIGH);
+  DIO_PinWrite(KEYPAD_PORT, ROW2, HIGH);
+  DIO_PinWrite(KEYPAD_PORT, ROW3, HIGH);
+}
+
+/**********************************************************************
+ * Function : KEYPAD_getKey()
+ *
+ *  Description:
+ *  This function is used to get the key pressed on the KEYPAD
+ * through applying the key-detection algorithm.
+ *  PRE-CONDITION: KEYPAD is initialized
+
+ *  @return key pressed.
+ **********************************************************************/
+vuint8_t KEYPAD_getKey() {
+  vuint8_t i, j = 0;
+  for (i = 0; i < 4; i++) {
+    DIO_PinWrite(KEYPAD_PORT, ROW0, HIGH);
+    DIO_PinWrite(KEYPAD_PORT, ROW1, HIGH);
+    DIO_PinWrite(KEYPAD_PORT, ROW2, HIGH);
+    DIO_PinWrite(KEYPAD_PORT, ROW3, HIGH);
+    DIO_PinWrite(KEYPAD_PORT, Keypad_Rows[i], LOW);
+    for (j = 0; j < 4; j++) {
+      if (!(DIO_PinRead(KEYPAD_PORT, Keypad_Cols[j]))) {
+        while (!(DIO_PinRead(KEYPAD_PORT, Keypad_Cols[j])))
+          ;
+        switch (i) {
+        case ROW0:
+          if (j == 0)
+            return '7';
+          if (j == 1)
+            return '8';
+          if (j == 2)
+            return '9';
+          if (j == 3)
+            return '/';
+          break;
+        case ROW1:
+          if (j == 0)
+            return '4';
+          if (j == 1)
+            return '5';
+          if (j == 2)
+            return '6';
+          if (j == 3)
+            return '*';
+          break;
+        case ROW2:
+          if (j == 0)
+            return '1';
+          if (j == 1)
+            return '2';
+          if (j == 2)
+            return '3';
+          if (j == 3)
+            return '-';
+          break;
+        case ROW3:
+          if (j == 0)
+            return '!';
+          if (j == 1)
+            return '0';
+          if (j == 2)
+            return '=';
+          if (j == 3)
+            return '+';
+          break;
+        default:
+          break;
+        }
+      }
+    }
+  }
+  return 'N';
+}
---
+++ Essential-Peripherals/Lab6/HAL/KEYPAD/KEYPAD_Config.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * Copyright (C) 2024 by Abdelrahman Kamal - Learn-In-Depth Diploma
+ *****************************************************************************/
+/**
+ * @file KEYPAD_Config.h
+ * @brief This module contains interface definitions for the
+ *  LCD configuration.
+ * @author Abdelrahman Kamal
+ *
+ */
+
+#ifndef KEYPAD_CONFIG_H
+#define KEYPAD_CONFIG_H
+
+/**********************************************************************
+ * Includes
+ **********************************************************************/
+#include "DIO.h"
+
+/**********************************************************************
+ * Preprocessor Macros
+ **********************************************************************/
+#define KEYPAD_PORT PORTD
+#define ROW0 0
+#define ROW1 1
+#define ROW2 2
+#define ROW3 3
+#define COL0 4
+#define COL1 5
+#define COL2 6
+#define COL3 7
+
+/**********************************************************************
+ * Typedefs
+ **********************************************************************/
+
+/**********************************************************************
+ * Function Prototypes
+ **********************************************************************/
+const ST_GpioConfig_t *const KEYPAD_ConfigGet(void);
+#endif /* KEYPAD_CONFIG_H */
