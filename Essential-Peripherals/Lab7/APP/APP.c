2ebdc49cde97a7f4caac9cddbfc5517bd4172ddc
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2024-07-02 15:39:58 +0300

    Application

--- Essential-Peripherals/Lab7/APP/APP.c
+++ Essential-Peripherals/Lab7/APP/APP.c
@@ -19,6 +19,27 @@ extern void delay_ms(vuint32_t time);
  * Function Definitions
  **********************************************************************/
 /**********************************************************************
+ * Function :MCAL/HAL_Init()
+ *
+ * Description:
+ *
+ * This function is used to initialize the mcal and hal layers
+ *
+ * PRE-CONDITION: The MCAL and HAL are configured.
+ *
+ * POST-CONDITION: The mcal/hal is initialized
+
+ **********************************************************************/
+void MCAL_Init() {
+  Clock_Init();
+  Gpio_Init(Gpio_ConfigGet());
+}
+
+void HAL_Init() {
+  LCD_Init();
+  KEYPAD_Init();
+}
+/**********************************************************************
  * Function :APP_Init()
  *
  * Description:
@@ -31,8 +52,18 @@ extern void delay_ms(vuint32_t time);
 
  **********************************************************************/
 void APP_Init(void) {
-  Clock_Init();
-  LCD_Init();
+  LCD_sendString((vuint8_t *)"Hello there!");
+  delay_ms(50);
+  LCD_clearScreen();
+  vuint8_t i = 0;
+  for (i = 0; i < 11; i++) {
+    LCD_sendNumber(i);
+    delay_ms(100);
+  }
+  LCD_clearScreen();
+  LCD_sendString((vuint8_t *)"Keypad is Ready");
+  delay_ms(500);
+  LCD_clearScreen();
 }
 /**********************************************************************
  * Function :APP_Start()
@@ -47,7 +78,14 @@ void APP_Init(void) {
 
  **********************************************************************/
 void APP_Start(void) {
-  LCD_sendString("Learn In Depth");
-  delay_ms(50);
-  LCD_clearScreen();
+  vuint8_t key;
+  key = KEYPAD_getKey();
+  switch (key) {
+  case '!':
+    LCD_clearScreen();
+  case 'N':
+    break;
+  default:
+    LCD_sendChar(key);
+  }
 }
