8804d3e8f330a8928e5920eb0ff43ca818d98d42
IamAbdelrahman <abdleruhman.m.kamal@gmail.com>
2024-11-25 13:07:19 +0200

    Add basic calculator using LCD and Keypad

---
+++ Essential-Peripherals/GPIO/Lab6/HAL/KEYPAD/KEYPAD_Config.c
@@ -0,0 +1,63 @@
+/******************************************************************************
+ * Copyright (C) 2024 by Abdelrahman Kamal - Learn-In-Depth Diploma
+ *****************************************************************************/
+/**
+ * @file KEYPAD_Config.c
+ * @brief This module contains the implementation for the KEYPAD configuration
+ *
+ * @author Abdelrahman Kamal
+ *
+ */
+/**********************************************************************
+ * Includes
+ **********************************************************************/
+#include "KEYPAD_Config.h"
+
+/**********************************************************************
+ * Preprocessor Macros
+ **********************************************************************/
+
+/**********************************************************************
+ * Typedefs
+ **********************************************************************/
+
+/**********************************************************************
+ * Module Variables
+ **********************************************************************/
+/**
+ * The following array contains the configuration data for each KEYPAD driver
+ * Each row represents a single pin.
+ * Each column is representing a member of the GpioConfig_t structure.
+ * This table is read in by KEYPAD_Init(), where each pin is then set up based
+ * on this table.
+ */
+const ST_GpioConfig_t KEYPADConfig[] = {
+    /*   PORT      PIN	   STATE    */
+    {KEYPAD_PORT, Pin0, DIO_OUTPUT},
+    {KEYPAD_PORT, Pin1, DIO_OUTPUT},
+    {KEYPAD_PORT, Pin2, DIO_OUTPUT},
+    {KEYPAD_PORT, Pin3, DIO_OUTPUT},
+    {KEYPAD_PORT, Pin4, DIO_INPUT_PULLUP},
+    {KEYPAD_PORT, Pin5, DIO_INPUT_PULLUP},
+    {KEYPAD_PORT, Pin6, DIO_INPUT_PULLUP},
+    {KEYPAD_PORT, Pin7, DIO_INPUT_PULLUP}};
+/**********************************************************************
+ * Function Definitions
+ **********************************************************************/
+/**********************************************************************
+ * Function : KEYPAD_ConfigGet()
+ *
+ * This function is used to initialize the KEYPAD based on the configuration
+ * table defined in LCDConfig module.
+ *
+ * PRE-CONDITION: Configuration table needs to populated (sizeof > 0)
+ *
+ * POST-CONDITION: A constant pointer to the first member of the
+ * configuration table will be returned.
+ *
+ ** @return A pointer to the configuration table.
+
+ **********************************************************************/
+const ST_GpioConfig_t *const KEYPAD_ConfigGet(void) {
+  return (ST_GpioConfig_t *const) & KEYPADConfig[0];
+}
---
+++ Essential-Peripherals/GPIO/Lab6/MCAL/DIO/DIO.h
@@ -0,0 +1,34 @@
+/******************************************************************************
+ * Copyright (C) 2024 by Abdelrahman Kamal - Learn-In-Depth Diploma
+ *****************************************************************************/
+/**
+ * @file DIO.h
+ * @brief The interface definition for the DIO
+ *
+ * This is the header file for the definition of the interface for a DIO
+ * peripheral on a standard microcontroller.
+ * @author Abdelrahman Kamal
+ *
+ */
+
+#ifndef DIO_H
+#define DIO_H
+/**********************************************************************
+ * Includes
+ **********************************************************************/
+#include "DIO_Config.h"
+
+/**********************************************************************
+ * Preprocessor Macros
+ **********************************************************************/
+#define TYPE vuint8_t
+
+/**********************************************************************
+ * Function Prototypes
+ **********************************************************************/
+void DIO_Init(const ST_GpioConfig_t *const Config);
+EN_State_t DIO_PinRead(EN_GpioX_t Port, EN_GpioPin_t Pin);
+void DIO_PinWrite(EN_GpioX_t Port, EN_GpioPin_t Pin, EN_State_t state);
+void DIO_WritePort(EN_GpioX_t Port, vuint8_t PortValue);
+
+#endif /* DIO_H */
